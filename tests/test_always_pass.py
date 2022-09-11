69792e6fd559407bcb95b009120616190e661854
RudolfHlavacek <70911572+RudolfHlavacek@users.noreply.github.com>
2022-09-11 20:58:06 +0200

    Update test_always_pass.py

--- tests/test_always_pass.py
+++ tests/test_always_pass.py
@@ -1,8 +1,8 @@
-def test_always_pass:
+def test_always_pass():
     """Always pass"""
     assert True
 
-def test_always_pass_three_times:
+def test_always_pass_three_times():
     assert True
     assert True
     assert True
